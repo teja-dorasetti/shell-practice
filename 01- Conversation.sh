e5835a58e34edd5fd95ec0dbc3658d7c82c047e8
Tejadorasetti <thedevops.learnerxgmail.com>
2026-01-30 14:19:58 +0530

    conversion

---
+++ 01- Conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "suresh :: hello ramesh how are you?"
+echo "ramesh:: Hi suresh i'm doing good thanks for asking. what about you?"
+echo "suresh:: I'm also doing great, what are you doing now a days?"
+echo "ramesh:: nothing much im started learning DevSecOps.What are you doing than?"
+echo "suresh:: I'm learning cyber security...."
