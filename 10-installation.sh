aabb1b36028af303a921afbfa6d545f981da50a0
Tejadorasetti <thedevops.learnerxgmail.com>
2026-01-31 21:59:54 +0530

    nginx

--- 10-installation.sh
+++ 10-installation.sh
@@ -6,11 +6,13 @@ if [ $USERID -ne 0 ]; then
     echo "this is not a root user to install Nginx"
 
 fi    
-    echo "Installing nginx"
 
-    dnf install nginx -y 
+echo "Installing nginx"
 
-if [$? -ne 0]; then
+dnf install nginx -y 
+
+if [ $? -ne 0 ]; then
+
     echo "Installing Nginx ....failure"    
     exit 1
 
