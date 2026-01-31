5db7d58844ba97cf225b1123b0e0499bdea0ee99
Tejadorasetti <thedevops.learnerxgmail.com>
2026-01-31 10:31:23 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PERSON1=suresh
-PERSON2=ramesh # dont give any type of gap in between value and =
+PERSON1=Trump
+PERSON2=Putin # dont give any type of gap in between value and =
 
-echo "$PERSON1 :: hellO $PERSON2 how are you?"
+echo "$PERSON1 :: hello $PERSON2 how are you?"
 echo "$PERSON2:: Hi $PERSON1 i'm doing good thanks for asking. what about you?"
 echo "$PERSON1:: I'm also doing great, what are you doing now a days?"
 echo "$PERSON2:: nothing much im started learning DevSecOps.What are you doing than?"
