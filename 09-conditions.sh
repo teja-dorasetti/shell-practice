016d1237455937ee1658ce42f0c070bd1b8c2993
Tejadorasetti <thedevops.learnerxgmail.com>
2026-01-31 20:23:49 +0530

    Conditions

---
+++ 09-conditions.sh
@@ -0,0 +1,16 @@
+#! bin/bash
+
+NUMBER=25
+
+### -gt -> greater than
+### -lt -> less than
+### -nt -> not equal
+### -eq -> equal 
+
+if [ $NUMBER -gt 20]; then
+     echo "Given Number: $NUMBER is grater than 20"
+elif [ $NUMBER -nq 20]; then     
+     echo "Given Number: $NUMBER is equal to 20"
+else 
+     echo "Given Number: $NUMBER is less than 20"
+fi
