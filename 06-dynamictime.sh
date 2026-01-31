27a32f61b7c027015bc747fe2b220e81d2241154
Tejadorasetti <thedevops.learnerxgmail.com>
2026-01-31 12:16:07 +0530

    DynamicTimeStamp

---
+++ 06-dynamictime.sh
@@ -0,0 +1,13 @@
+#! bin/bash
+
+START_TIME= $(date +%s)
+
+echo "Script executed at : $START_TIME"
+
+sleep 10
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+
+echo "script executed in: $TOTAL_TME seconds"
