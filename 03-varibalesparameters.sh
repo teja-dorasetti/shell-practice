84423e9725774def4c49ebb0f9b46b3fbda926f0
Tejadorasetti <thedevops.learnerxgmail.com>
2026-01-31 10:42:46 +0530

    parameters

--- 03-varibalesparameters.sh
+++ 03-varibalesparameters.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2 # dont give any type of gap in between value and =
 
-echo "$PERSON1 :: hello $PERSON2 how are you?"
+echo "$PERSON1:: hello $PERSON2 how are you?"
 echo "$PERSON2:: Hi $PERSON1 i'm doing good thanks for asking. what about you?"
 echo "$PERSON1:: I'm also doing great, what are you doing now a days?"
 echo "$PERSON2:: nothing much im started learning DevSecOps.What are you doing than?"
