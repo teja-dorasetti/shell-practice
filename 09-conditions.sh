d67e48e94f67e4dd8be7d085e9bdb67646383d77
Tejadorasetti <thedevops.learnerxgmail.com>
2026-01-31 20:27:20 +0530

    Conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -8,8 +8,8 @@ NUMBER=25
 ### -eq -> equal 
 
 if [ $NUMBER -gt 20]; then
-     echo "Given Number: $NUMBER is grater than 20"
-elif [ $NUMBER -nq 20]; then     
+     echo "Given Number: $NUMBER is greater than 20"
+elif [ $NUMBER -eq 20]; then     
      echo "Given Number: $NUMBER is equal to 20"
 else 
      echo "Given Number: $NUMBER is less than 20"
