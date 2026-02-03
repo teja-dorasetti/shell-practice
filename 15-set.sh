502fc750a53969a6175b90559b9c969e9832cb0d
Tejadorasetti <thedevops.learnerxgmail.com>
2026-02-03 14:07:34 +0530

    Loops

---
+++ 15-set.sh
@@ -0,0 +1,42 @@
+#! bin/bash
+
+set -e # exit on error
+
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+
+fi    
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+
+        echo -e " $2 ....$R failure $N"   | tee -a $LOGS_FILE
+        exit 1
+
+    else 
+        echo -e "$2....$G Success $N"    | tee -a $LOGS_FILE
+    fi
+
+}
+    
+for package in $@ # $@ means all agruments passed [ sudo sh 13-loops.sh nginx nodejs mysql]
+do
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo -e "$Y $package is not installed,installing now $N"
+        dnf install $package -y &>>$LOGS_FILE
+        #VALIDATE $? "$package installation"
+    else 
+        echo -e "$G $package already installed,skipping... $N"   
+    fi     
+done
+
