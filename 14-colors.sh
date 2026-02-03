155ffb7ed2d1082f493bda9a6a7caa760fea623c
Tejadorasetti <thedevops.learnerxgmail.com>
2026-02-03 13:02:24 +0530

    colors

--- 14-colors.sh
+++ 14-colors.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 
-## R= "\e[31m"
+R= "\e[31m"
 ## G= "\e[32m"
 ## Y= "\e[33m"
-## B= "\e[34m"
+B= "\e[34m"
 ## M= "\e[35m"
 ## C= "\e[36m"
 ## W= "\e[37m"
-## N= "\e[0m"
+N= "\e[0m"
 
 echo -e "$R Hello world $N, $B I'm learning linux shell scripting $N"
-echo -e "$G Today is a great day $N to learn something new"
-echo -e "$Y Colors in shell scripting $N are very easy to use"
