91c5a1a7056479888660f3840cc473679b9c2d43
Tejadorasetti <thedevops.learnerxgmail.com>
2026-02-03 11:25:13 +0530

    Loops

--- 13-Loops.sh
+++ 13-Loops.sh
@@ -26,12 +26,12 @@ VALIDATE(){
     
 for package in $@ # $@ means all agruments passed [ sudo sh 13-loops.sh nginx nodejs mysql]
 do
-    dnf list installed $package &>> $LOGS_FILE
+    dnf list installed $package &>>$LOGS_FILE
     if [ $? -ne 0]; then
-        echo "$package is not installed, installing now"
-        dnf install $package -y &>> $LOGS_FILE
+        echo "$package is not installed,installing now"
+        dnf install $package -y &>>$LOGS_FILE
         VALIDATE $? "$package installation"
     else 
-        echo "$package already installed, skipping..."   
+        echo "$package already installed,skipping..."   
     fi     
 done
