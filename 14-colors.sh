80839511c102f22cbfd5b2822110b1612af421be
Tejadorasetti <thedevops.learnerxgmail.com>
2026-02-03 13:10:07 +0530

    Loops

--- 14-colors.sh
+++ 14-colors.sh
@@ -3,10 +3,6 @@
 R= "\e[31m"
 G= "\e[32m"
 Y= "\e[33m"
-B= "\e[34m"
-M= "\e[35m"
-C= "\e[36m"
-W= "\e[37m"
 N= "\e[0m"
 
-echo -e "$R Hello world $N, $B I'm learning linux shell scripting $N"
+echo -e "$R Hello world $N, $G I'm learning linux shell scripting $N"
