abd83bbbc9ce0e6b41f456e8c23dc795156460c0
Tejadorasetti <thedevops.learnerxgmail.com>
2026-02-03 12:45:35 +0530

    colors

---
+++ 14-colors.sh
@@ -0,0 +1,12 @@
+#! bin/bash
+
+R= "\e[31m"
+G= "\e[32m"
+Y= "\e[33m"
+B= "\e[34m"
+M= "\e[35m"
+C= "\e[36m"
+W= "\e[37m"
+NC= "\e[0m"
+
+echo -e "$R hello world $NC, $B I'm learning linux shell scripting $NC"
