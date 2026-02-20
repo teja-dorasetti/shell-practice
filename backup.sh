f14a3ec20b7b0c8167cacca5a1eb0d6c925fe665
Tejadorasetti <thedevops.learnerxgmail.com>
2026-02-20 09:42:23 +0530

    actions

--- backup.sh
+++ backup.sh
@@ -50,7 +50,7 @@ log "Source Directory: $SOURCE_DIR"
 log "Destination Directory: $DEST_DIRECTORY"    
 log "Days: $DAYS"
 
-if [ -z "$FILES" ]; then
+if [ -z $FILES ]; then
     log "No files found for backup"
     exit 0
 fi  
